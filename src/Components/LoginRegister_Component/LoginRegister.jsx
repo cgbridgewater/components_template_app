3a8eeec4c21b8990d65ca7c5ac6c323ca6121a88
cgbridgewater <cgbridgewater@outlook.com>
2024-07-02 15:59:28 -0700

    start on login

--- src/Components/LoginRegister_Component/LoginRegister.jsx
+++ src/Components/LoginRegister_Component/LoginRegister.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import { FaUser, FaLock } from "react-icons/fa";
 
 const LoginRegister = () => {
   return (
     <main>
-        <div className="wrapper">
-            
+        <div className="form_wrapper">
+            <div className="form-box login">
+                <form action="">
+                    <h1>Login</h1>
+
+                    <div className="input_box"> 
+                        <input type="text" required />
+                        <FaUser className="icon" />
+                    </div>
+
+                    <div className="input_box"> 
+                        <input type="password" required />
+                        <FaLock className="icon" />
+                    </div>
+
+                    <div className="remember-forgot">
+                        <label htmlFor="">
+                            <input type="checkbox" />Remember Me
+                        </label>
+                        <a href="#">Forgot Password</a>
+                    </div>
+
+                    <button type="submit">Login</button>
+
+                    <div className="register-link">
+                        <p>
+                            Don't have an account? <a href="#">Register</a>
+                        </p>
+                    </div>
+                </form>
+            </div>
         </div>
     </main>
   )
