4d369c81f0bc0435dc5e3878c0e18a815316d470
cgbridgewater <cgbridgewater@outlook.com>
2024-07-09 18:22:14 -0700

    update to control state on forms and checkbox

--- src/Components/LoginRegister_Component/LoginRegister.jsx
+++ src/Components/LoginRegister_Component/LoginRegister.jsx
@@ -1,33 +1,92 @@
 import React, { useState } from 'react'
 import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 const LoginRegister = () => {
 
-    const [action, setAction] = useState("");
-    const [regname, setRegname] = useState("");
-    const [regemail, setRegemail] = useState("");
-    const [regpass, setRegpass] = useState("");
-    const [regconfpass, setRegconfpass] = useState("");
-    const [signinname, setSigninname] = useState("");
-    const [signinpass, setSigninpass] = useState("");
+    const navigate = useNavigate();
 
-    // Function to slide to Signup side and clear login form out
+    //                                 //
+    // REGISTRATION STATE AND HANDLING //
+    //                                 //
+    const [registrationForm, setRegistrationForm] = useState({
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
+
+    const handleRegistrationForm =(e) => {
+        // props.setAuthorized(""); //use this to set auth via props
+        setRegistrationForm({ ...registrationForm, [e.target.name]: e.target.value });
+    }
+    //                                     //
+    // END REGISTRATION STATE AND HANDLING //
+    //                                     //
+
+
+    //                          //
+    // LOGIN STATE AND HANDLING //
+    //                          //
+    const [loginForm, setLoginForm] = useState({
+        email: "",
+        password: ""
+    });
+    
+    const handleLogin = (e) => {
+        // props.setAuthorized(""); //use this to set auth via props
+        setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
+    }
+    //                              //
+    // END LOGIN STATE AND HANDLING //
+    //                              //
+
+
+    //                     //
+    // FORM TOGGLE CONTROL //
+    //                     //
+    // FORM TOGGLE STATE
+    const [action, setAction] = useState("");
+
+    // AGREE TO T&C  STATE AND CHANGE HANDLER
+    const [agree, setAgree] = useState(false);
+    const handleAgreeChange = (e) => {
+        setAgree(e.target.checked);
+    }
+
+    //  REMEMBER ME STATE AND CHANGE HANDLER
+    const [remember, setRemember] = useState(false);
+    const handleRememberChange = (e) => {
+        setRemember(e.target.checked);
+    }
+
+    // HANDLE REGISTER LINK CLICK
     const register_link = (e) => {
         e.preventDefault();
-        setSigninname("");
-        setSigninpass("");
         setAction(" active");
+        setLoginForm({
+            email: "",
+            password: ""
+        });
+        setAgree(false);
+        setRemember(false);
     }
 
-    // Function to slide to login side and clear signup form out
+    // HANDLE LOGIN LINK CLICK
     const login_link = (e) => {
         e.preventDefault();
-        setRegname("");
-        setRegemail("");
-        setRegpass("");
-        setRegconfpass("");
         setAction("");
+        setRegistrationForm({
+            email: "",
+            password: "",
+            confirmPassword: ""
+        });
+        setAgree(false);
+        setRemember(false);
     }
+    //                         //
+    // END FORM TOGGLE CONTROL //
+    //                         //
+
 
     return (
         <main>
@@ -39,20 +98,34 @@ const LoginRegister = () => {
                         <form action="">
                             {/* HEADER */}
                             <h1>Login</h1>
-                            {/* USERNAME INPUT */}
+                            {/* EMAIL INPUT */}
                             <div className="input_box"> 
-                                <input type="text" placeholder="Username" required />
-                                <FaUser className="icon" />
+                                <input 
+                                    onChange={handleLogin}
+                                    type="email" 
+                                    name="email"
+                                    placeholder="Email" 
+                                    value={loginForm.email}
+                                    required 
+                                />
+                                <FaEnvelope className="icon" />
                             </div>
                             {/* PASSWORD INPUT */}
                             <div className="input_box"> 
-                                <input type="password" placeholder="Password" required />
+                                <input
+                                    onChange={handleLogin}
+                                    type="password" 
+                                    name="password"
+                                    placeholder="Password"
+                                    value={loginForm.password}
+                                    required 
+                                />
                                 <FaLock className="icon" />
                             </div>
                             {/* REMEMBER ME CHECK */}
                             <div className="remember_forgot">
                                 <label htmlFor="remember_me">
-                                    <input id="remember_me" type="checkbox" />Remember Me
+                                    <input id="remember_me" type="checkbox" onChange={handleRememberChange} checked={remember} />Remember Me
                                 </label>
                                 <a  href="#"><span className="form_link">Forgot Password</span></a>
                             </div>
@@ -66,41 +139,57 @@ const LoginRegister = () => {
                             </div>
                         </form>
                     </div>
-
+                    {/* END LOGIN FORM */}
                     {/* REGISTER FORM */}
                     <div className="form_box register">
                         <form action="">
                             {/* HEADER */}
                             <h1>Sign Up</h1>
-                            {/* USERNAME INPUT */}
-                            <div className="input_box"> 
-                                <input type="text" placeholder="Username" required />
-                                <FaUser className="icon" />
-                            </div>
                             {/* EMAIL INPUT */}
                             <div className="input_box"> 
-                                <input type="email" placeholder="Email" required />
+                                <input 
+                                    onChange={handleRegistrationForm}
+                                    type="email" 
+                                    name="email"
+                                    placeholder="Email" 
+                                    value={registrationForm.email}
+                                    required 
+                                />
                                 <FaEnvelope className="icon" />
                             </div>
                             {/* PASSWORD INPUT */}
                             <div className="input_box"> 
-                                <input type="password" placeholder="Password" required />
+                                <input
+                                    onChange={handleRegistrationForm}
+                                    type="password" 
+                                    name="password"
+                                    placeholder="Password"
+                                    value={registrationForm.password}
+                                    required 
+                                />
                                 <FaLock className="icon" />
                             </div>
                             {/* CONFIRM PASSWORD INPUT */}
                             <div className="input_box"> 
-                                <input type="password" placeholder="Confirm Password" required />
+                                <input 
+                                    onChange={handleRegistrationForm}
+                                    type="password" 
+                                    name="confirmPassword"
+                                    placeholder="Confirm Password"
+                                    value={registrationForm.confirmPassword}
+                                    required 
+                                />
                                 <FaLock className="icon" />
                             </div>
                             {/* AGREE TO TERMS CHECKBOX */}
                             <div className="remember_forgot">
                                 <label htmlFor="i_agree">
-                                    <input id="i_agree" type="checkbox" />I agree to the&nbsp;
+                                    <input id="i_agree" type="checkbox" onChange={handleAgreeChange} checked={agree}/>I agree to the&nbsp;
                                     <a href="#"><span className="form_link">Terms & Conditions</span></a>
                                 </label>
                             </div>
                             {/* SUBMIT BBUTTON */}
-                            <button type="submit"><span>Sign Up</span></button>
+                            <button type="submit" disabled={!agree}><span>Sign Up</span></button>
                             {/* SIGNUP LINK */}
                             <div className="register_link">
                                 <p>
@@ -109,6 +198,7 @@ const LoginRegister = () => {
                             </div>
                         </form>
                     </div>
+                    {/* END REGISTER FORM */}
                 </div>
             </div>
         </main>
