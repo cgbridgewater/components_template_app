87f3c046b0d703a14d907abdbaa0c85845517955
cgbridgewater <cgbridgewater@outlook.com>
2024-07-25 18:43:00 -0700

    clean up

--- src/Views/Login.jsx
+++ src/Views/Login.jsx
@@ -1,16 +1,12 @@
 import LoginRegister from "../Components/LoginRegister_Component/LoginRegister"
-import Modal_Component from "../Components/Modal_Component/Modal_Component"
 
 const Login = () => {
 
-return (
-    <>
-        <LoginRegister />
-        <Modal_Component />
-    </>
-)
-
-
+    return (
+        <>
+            <LoginRegister />
+        </>
+    )
 }
 
 export default Login
