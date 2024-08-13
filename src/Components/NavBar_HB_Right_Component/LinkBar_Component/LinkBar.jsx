72c817df1860640c05810b9c6f23d7414f5d7577
cgbridgewater <cgbridgewater@outlook.com>
2024-08-12 18:42:44 -0700

    add socials to link bar

--- src/Components/NavBar_HB_Right_Component/LinkBar_Component/LinkBar.jsx
+++ src/Components/NavBar_HB_Right_Component/LinkBar_Component/LinkBar.jsx
@@ -1,19 +1,28 @@
 import { Link } from "react-router-dom";
+import { FaInstagramSquare, FaFacebookSquare, FaLinkedin } from "react-icons/fa";
+
 
 const LinkBar = () => {
 
     return (
-
         <div className="LinkBar">
-            <ul>
-                <li><Link className="LinkBar_Link">Home</Link></li>
-                <li><Link className="LinkBar_Link">About</Link></li>
-                <li><Link className="LinkBar_Link">Services</Link></li>
-                <li><Link className="LinkBar_Link">Projects</Link></li>
-                <li><Link className="LinkBar_Link">Contact</Link></li>
-            </ul>
+            <div className="Links_LinkBar">
+                <ul>
+                    <li><Link className="LinkBar_Link">Home</Link></li>
+                    <li><Link className="LinkBar_Link">About</Link></li>
+                    <li><Link className="LinkBar_Link">Services</Link></li>
+                    <li><Link className="LinkBar_Link">Projects</Link></li>
+                    <li><Link className="LinkBar_Link">Contact</Link></li>
+                </ul>
+            </div>
+            <div className="Socials_LinkBar">
+                <ul>
+                    <li><Link className="Socials_Link"><FaInstagramSquare /></Link></li>
+                    <li><Link className="Socials_Link"><FaLinkedin /></Link></li>
+                    <li><Link className="Socials_Link"><FaFacebookSquare /></Link></li>
+                </ul>
+            </div>
         </div>
-
     );
 };
 
